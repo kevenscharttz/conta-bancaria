25a7d96939ea0fa9f1a11bb4e139c54ba13496a0
Keven Willians <149530293+kevenscharttz@users.noreply.github.com>
2025-04-10 11:19:41 -0300

    Adição do projeto

---
+++ ContaBancaria/Main.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        MetodosConta metodo = new MetodosConta();
+        Scanner ler = new Scanner(System.in);
+
+        //infos iniciais
+        String nome = "Keven Willians";
+        String tipoConta = "Corrente";
+        double saldoConta = 2500;
+        int opcao;
+
+        System.out.printf("""
+                *************************************************
+                SEJA BEM VINDO SENHOR(A) %s
+                
+                Dados iniciais do cliente:
+                
+                Nome:               %s
+                Tipo de conta:      %s
+                Saldo inicial:      %.2f
+                *************************************************
+                
+                %n""", nome, nome, tipoConta, saldoConta);
+
+        System.out.println("""
+                Operações
+                
+                1- Consultar valor
+                2- Receber valor
+                3- Transferir valor
+                4- Sair
+                """);
+
+        opcao = ler.nextInt();
+        while (opcao > 0){
+            switch (opcao) {
+                case 1:
+                    System.out.println("Você possui R$"+saldoConta);
+                    break;
+                case 2:
+                    saldoConta = metodo.receberSaldos (saldoConta, ler);
+                    break;
+                case 3:
+                    saldoConta = metodo.transferirSaldos (saldoConta, ler);
+                    break;
+                case 4:
+                    System.out.println("Encerrando o sistema. Obrigado!");
+                    return;
+
+                    default:
+                    System.out.println("Opção invalida\n");
+                    break;
+            }
+            System.out.println("""
+                \nOperações
+                
+                1- Consultar valor
+                2- Receber valor
+                3- Transferir valor
+                4- Sair
+                """);
+            opcao = ler.nextInt();
+        }
+    }
+}
